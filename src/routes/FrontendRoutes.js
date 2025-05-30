0baf26c236eb769f642ca4f7fbdc50831c2dc4a7
AnthonyWGit <anthony.wetzstein@gmail.com>
2025-05-30 23:35:04 +0200

    dropping static routes and routing to every page

--- src/routes/FrontendRoutes.js
+++ src/routes/FrontendRoutes.js
@@ -5,12 +5,13 @@ import staticController from '../controllers/StaticController.js';
 
 // Static HTML Routes
 
-router.get('/random', (req, res) => res.render('templates/jokeUnique'));
+// router.get('/random', (req, res) => res.render('templates/jokeRandom'));
 
 // Dynamic Routes
+router.get('/jokeRandom', frontendController.showRandomPage);
 router.get('/jokes',frontendController.allJokesPage);
-router.get('/jokeUnique', frontendController.showJokeSelection);
-router.get('/jokeUnique/:jokeId', frontendController.showSelectedJoke);
+router.get('/jokeUnique', frontendController.showJokeSelectionPage);
+router.get('/jokeUnique/:jokeId', frontendController.showSelectedJokePage);
 router.get('/home', frontendController.home);
 
 // Redirect
