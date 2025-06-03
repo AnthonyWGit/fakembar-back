13f52d1d52cbf25a75c2f4fb9c4ffe79ea7b97e8
AnthonyWGit <anthony.wetzstein@gmail.com>
2025-06-03 14:13:55 +0200

    seeders updated with jokes

--- seeders/seed-jokes.js
+++ seeders/seed-jokes.js
@@ -20,6 +20,53 @@ module.exports = {
           createdAt: new Date(),
           updatedAt: new Date()
         },
+                {
+          text: "test",
+          answer: "test",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+                {
+          text: "Pourquoi le football c'est rigolo ?",
+          answer: "Parce que Thierry en rit",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+                {
+          text: "Quel est le sport le plus fruité ?",
+          answer: "La boxe, parce que tu te prends des pêches dans la poire et tu tombes dans les pommes.",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          text: "Que se fait un Schtroumpf quand il tombe ?",
+          answer: "Un Bleu",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          text: "Quel est le comble pour un marin ?",
+          answer: "Avoir le nez qui coule",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          text: "Qu'est ce que les enfants usent le plus à l'école ?",
+          answer: "Le professeur",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          text: "Quel est le sport le plus silencieux ?",
+          answer: "Le para-chuuuut",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },        {
+          text: "Quel est le comble pour un joueur de bowling ?",
+          answer: "C’est de perdre la boule",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
         // Add others jokes here
       ], {});
     } catch (error) {
